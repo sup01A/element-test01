ab417c062fcce72cf744e154d25b455ed7155045
oushubo_redmi <a13077548058@163.com>
2024-09-05 19:31:23 +0800

    444

--- src/views/article/ArticleManage.vue
+++ src/views/article/ArticleManage.vue
@@ -3,14 +3,44 @@
 </script>
 
 <template>
-<div class="pagea">
-  文章管理
-</div>
+  <el-card style="width: 100%;min-height: 100vh">
+    <template #header>
+      <div style="display: flex;justify-content: space-between">
+        <span>文章分类</span>
+        <el-button type="primary">添加文章</el-button>
+      </div>
+    </template>
+    <el-form :inline="true">
+      <el-form-item label="文章分类">
+        <el-select
+            placeholder="请选择"
+            clearable
+            style="width: 150px"
+        >
+          <el-option label="Zone one" value="shanghai" />
+          <el-option label="Zone two" value="beijing" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="发布状态">
+        <el-select
+            placeholder="请选择"
+            clearable
+            style="width: 150px"
+        >
+          <el-option label="Zone one" value="shanghai" />
+          <el-option label="Zone two" value="beijing" />
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">搜索</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button>重置</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
 
 </template>
 
 <style scoped>
-.pagea {
-  min-height: 100%;
-}
 </style>
